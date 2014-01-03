764341d820593b0f45a44785b532ab121cffd39a
websterzhao <weizhao7810@hotmail.com>
2014-01-02 23:57:14 -0500

    unique bst DP update

--- workspace/Zcode/src/tree/UniqueBST.java
+++ workspace/Zcode/src/tree/UniqueBST.java
@@ -11,4 +11,41 @@ public class UniqueBST {
 	        return sum;
 	        
 	    }
+
+	   
+	   //DP
+	public int numTrees1(int n) {
+		if (n < 1)
+			return 0;
+		else if (n < 3)
+			return n;
+		else if (n == 3)
+			return 5;
+
+		int[] numNodes = new int[n + 1];
+		numNodes[0] = 1;
+		numNodes[1] = 1;
+		numNodes[2] = 2;
+		numNodes[3] = 5;
+
+		for (int i = 4; i <= n; i++) {
+			int num = 0;
+			int j = 1;
+			for (; j <= i / 2; j++) {
+				int leftnodes = numNodes[j - 1];
+				int rightnodes = numNodes[i - j];
+				num = num + (leftnodes * rightnodes);
+			}
+			num *= 2;
+			if (i % 2 != 0) {
+				int lfn = numNodes[j - 1];
+				int rhn = numNodes[i - j];
+				num = num + (lfn * rhn);
+			}
+
+			numNodes[i] = num;
+		}
+
+		return numNodes[n];
+	}
 }
