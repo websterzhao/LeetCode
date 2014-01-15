d039e158182f69729ddaa070f9d9c82432f4401b
websterzhao <weizhao7810@hotmail.com>
2014-01-14 23:40:53 -0500

    permutation sequence update

--- workspace/Zcode/src/recursion/PermutationSequence.java
+++ workspace/Zcode/src/recursion/PermutationSequence.java
@@ -1,49 +1,36 @@
 package recursion;
 
-import java.util.ArrayList;
-
 public class PermutationSequence {
-	   public String getPermutation(int n, int k) {
-	        ArrayList<ArrayList<Integer>> ret = new ArrayList<ArrayList<Integer>>();
-	        int[] num= new int[n];
-	        for(int i=1;i<=n;i++){
-	            num[i-1]=i;
-	        }
-	        perm(num,ret,0,n);
-	        ArrayList<Integer> arry=ret.get(k-1);
-	        StringBuilder kkk=new StringBuilder();
-	        for(int i:arry){
-	            kkk.append((char)('0'+i));
-	        }
-	        return kkk.toString();
-	     }
-	     private void perm(int[] num, ArrayList<ArrayList<Integer>> ret, int i,int n){
-	         if(i==n){
-	             ArrayList<Integer> temp=new ArrayList<Integer>();
-	             for(int e:num){
-	                 temp.add(e);
-	             }
-	             ret.add(temp);
-	             return;
-	         }
-	         for(int k=i;k<n;k++){
-	             if(needswap(num,i,k))  continue;
-	             swap(num,i,k);
-	             perm(num,ret,i+1,num.length);
-	             swap(num,i,k);
-	         }
-	     }
-	     private boolean needswap(int[] num, int i, int k){
-	        for (int j=i;j<k;j++){
-	            if (num[j]==num[k]){
-	                return true;
-	            }
-	        }
-	        return false;
-	     }
-	     private void swap(int[] num, int i, int k){
-	         int temp=num[k];
-	         num[k]=num[i];
-	         num[i]= temp;
-	     }
+	public String getPermutation(int n, int k) {
+		int[] nf = { 1, 2, 6, 24, 120, 720, 5040, 40320, 362880 }; // n factorail
+		char[] sb= new char[n];// result string
+		for (int i = 1; i <= n; i++) {   // initialization
+			sb[i-1] = (char) ((i + '0'));
+		} 
+		k--;
+		for (int i = 0; i < n; i++) {
+			int m = k % nf[n - i - 1];
+			int s = k / nf[n - i - 1];
+			if (m == 0 && s == 0) {
+				break;
+			} else {
+				if (s > 0) {
+					for (int j = i - 1 + s; j > i - 1; j--) {
+						char tmp = sb[j];
+						sb[j] = sb[j - 1];
+						sb[j - 1] = tmp;
+					}
+					if (m == 0) {
+						break;
+					}
+				}
+				k = m;
+			}
+		}
+		StringBuilder ssb = new StringBuilder();
+		for (int i = 0; i < n; i++) {  
+			ssb = ssb.append(sb[i]);
+		}
+		return ssb.toString();
+	}
 }
