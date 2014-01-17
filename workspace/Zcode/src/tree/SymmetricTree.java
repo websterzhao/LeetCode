2bddd9eb244bfea67b669b55317884bfd59f83a3
websterzhao <weizhao7810@hotmail.com>
2014-01-16 23:28:19 -0500

    symmetric Tree iterative way

--- workspace/Zcode/src/tree/SymmetricTree.java
+++ workspace/Zcode/src/tree/SymmetricTree.java
@@ -1,5 +1,7 @@
 package tree;
 
+import java.util.Stack;
+
 import anode.TreeNode;
 
 public class SymmetricTree {
@@ -21,6 +23,24 @@ public class SymmetricTree {
 	}
 	
 	
-	
+	 public boolean isSymmetric1(TreeNode root) {
+		 if (root == null) return true;
+		     Stack<TreeNode> s1 = new Stack<TreeNode>();
+		     Stack<TreeNode> s2 = new Stack<TreeNode>();
+		     s1.push(root.left);
+		     s2.push(root.right);
+		     while (!s1.empty() && !s2.empty()) {
+		         TreeNode n1 = s1.pop();
+		         TreeNode n2 = s2.pop();
+		         if (n1 == null && n2 == null) continue;
+		         if (n1 == null || n2 == null) return false;
+		         if (n1.val != n2.val) return false;
+		         s1.push(n1.left);
+		         s2.push(n2.right);
+		         s1.push(n1.right);
+		         s2.push(n2.left);
+		     }
+		     return true;
+		 }
 	
 }
