80c57a5b8f1bb4e410d96b57edb240870eb7675d
websterzhao <weizhao7810@hotmail.com>
2014-01-19 03:00:36 -0500

    update

--- workspace/Zcode/src/tree/RecoverBST.java
+++ workspace/Zcode/src/tree/RecoverBST.java
@@ -3,43 +3,40 @@ package tree;
 
 import anode.TreeNode;
 
-public class RecoverBST {    
-	TreeNode firstNode;
-    TreeNode secondNode;
-    public void recoverTree(TreeNode root) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
-        firstNode =null;
-        secondNode = null;
-        inorder(root,null);
-        swap(firstNode,secondNode);
+public class RecoverBST {   
+   TreeNode firstNode;
+   TreeNode secondNode;
+   public void recoverTree(TreeNode root) {
+       firstNode =null;
+       secondNode = null;
+       inorder(root,null);
+       swap(firstNode,secondNode);
 
 
-    }
+   }
 
-    public void swap(TreeNode f,TreeNode s){
-            int temp = f.val;
-            f.val = s.val;
-            s.val = temp;
-    }
+   public void swap(TreeNode f,TreeNode s){
+           int temp = f.val;
+           f.val = s.val;
+           s.val = temp;
+   }
 
-    public TreeNode inorder(TreeNode node, TreeNode prev){
-            if(node==null) return prev;
+   public TreeNode inorder(TreeNode node, TreeNode prev){
+           if(node==null) return prev;
 
-            TreeNode p = inorder(node.left,prev);
+           TreeNode p = inorder(node.left,prev);
 
-            if(p!=null && p.val>node.val){
-                    if(firstNode==null){
-                            firstNode = p;
-                            secondNode = node;
-                    }
-                    else
-                            secondNode = node;
-            }
+           if(p!=null && p.val>node.val){
+                   if(firstNode==null){
+                           firstNode = p;
+                           secondNode = node;
+                   }
+                   else
+                           secondNode = node;
+           }
 
-            p = inorder(node.right,node);
+           p = inorder(node.right,node);
 
-            return p;
-    }
-	 
+           return p;
+   }
 }
