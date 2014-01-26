712e1741057ae1302a90758b969884f344eb557a
websterzhao <weizhao7810@hotmail.com>
2014-01-26 01:09:11 -0500

    SingleNumberII.java

---
+++ workspace/Zcode/src/math/SingleNumberII.java
@@ -0,0 +1,15 @@
+package math;
+
+public class SingleNumberII {
+	public int singleNumber(int[] A) {
+		int ones = 0, twos = 0, threes = 0;
+		for (int i = 0; i < A.length; i++) {
+			twos |= ones & A[i];
+			ones ^= A[i];
+			threes = ones & twos;
+			ones &= ~threes;
+			twos &= ~threes;
+		}
+		return ones;
+	}
+}
