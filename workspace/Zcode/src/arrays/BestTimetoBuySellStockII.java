7a6a3b6a62cc147f105d5b537242b69f958b099c
websterzhao <weizhao7810@hotmail.com>
2014-01-27 23:24:59 -0500

    best time stockII

--- workspace/Zcode/src/arrays/BestTimetoBuySellStockII.java
+++ workspace/Zcode/src/arrays/BestTimetoBuySellStockII.java
@@ -1,6 +1,13 @@
 package arrays;
 
 public class BestTimetoBuySellStockII {
+/*
+* Scan the vector, add all the sub set which are non-decreasing order.
+e.g. 
+1 2 4 2 5 7 2 4 9 0 9
+(1 2 4) (2 5 7) ( 2 4 9) (0 9)  
+prof = 3 + 5 + 7 + 9  = 24.
+ */
 	public int maxProfit(int[] prices) {
 		int total = 0;
 	    for (int i=0; i< prices.length-1; i++) {
