d2eccd01dc72d35e03335ecd8d5aea9ee83cb8e1
websterzhao <weizhao7810@hotmail.com>
2014-01-27 22:31:04 -0500

    best time stock

--- workspace/Zcode/src/arrays/BestTimetoBuySellStock.java
+++ workspace/Zcode/src/arrays/BestTimetoBuySellStock.java
@@ -2,22 +2,15 @@ package arrays;
 
 public class BestTimetoBuySellStock {
     public int maxProfit(int[] prices) {
-        if(prices.length==0) return 0;
-        int n = prices.length;
-	       int[] min = new int[n];
-	       min[0] = 0;
-	       for(int i =1;i<n;i++){
-	    	   if(prices[i]<prices[min[i-1]])
-	    		   min[i]=i;
-	    	   else
-	    		   min[i]=min[i-1];
-	       }
-	       int ret=0;
-	       for(int i =1;i<n;i++){
-	    	   int temp = prices[i]-prices[min[i]];
-	    	   if(temp>ret)
-	    		   ret=temp;
-	       }
-	       return ret;
+    	  if(prices.length==0) return 0;
+          int min=prices[0];
+          int ret=0;
+          for(int i=1;i<prices.length;i++){
+              if(prices[i]<min)
+                  min=prices[i];
+              else
+                  ret=(prices[i]-min)>ret?(prices[i]-min):ret;
+          }
+          return ret;
 	    }
 }
